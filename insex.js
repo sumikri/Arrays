9fe615155b82218a8a8dee4905476b37a5d8f649
Sumi Kumari <sumiK9784@gmail.com>
2024-10-16 22:33:13 +0530

    first commit

---
+++ insex.js
@@ -0,0 +1,8 @@
+// Create an array
+let fruits = ['Apple', 'Banana', 'Orange'];
+console.log(fruits); // Output: ['Apple', 'Banana', 'Orange']
+
+
+// Access an element
+console.log(fruits[0]); // Output: 'Apple'
+console.log(fruits[1]); // Output: 'Banana'
